e46b8c6b67dfead32ec41d4557c9ad6b4f446eed
ekremekc <ekrem.ekici70@hotmail.com>
2025-05-29 20:21:53 +0300

    torque calculation

--- aiac2025/PlotsDir/plot.py
+++ aiac2025/PlotsDir/plot.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib.pyplot as plt
-
+import matplotlib
+matplotlib.rcParams.update({'font.size': 18})
 # Arrays to store data
 x_vals = []
 y_vals = []
@@ -18,7 +19,7 @@ for filename in sorted(os.listdir(".")):
 
 # Plotting
 plt.plot(x_vals, y_vals, 'o-', label='T_diff')
-plt.xlabel("Thickness")
+plt.xlabel("Thickness (m)")
 plt.ylabel("Temperature Difference (C)")
 # plt.title("T_diff vs Thickness")
 plt.grid(True)
@@ -26,4 +27,32 @@ plt.grid(True)
 plt.gca().invert_xaxis()
 plt.tight_layout()
 plt.savefig("T_diff.png")
+# plt.show()
+plt.close()
+
+import numpy as np
+
+def p_contact(T, N_c, f, d_c, A_contact):
+    P = T*N_c/(f*d_c*A_contact)
+    return P
+
+def calculate_torque(P, N_c, f, d_c, A_contact):
+    T = P*(f*d_c*A_contact)/N_c
+    return T
+
+# T = np.linspace(1000, 10000, 20) #N
+N_c = 4
+f = 0.2
+d_c = 5 #mm
+A_contact = np.pi*d_c**2/4
+Ps = np.array([34, 69, 172, 345])*1E-3
+Ts = calculate_torque(Ps, N_c, f, d_c, A_contact)
+print(Ts)
+
+plt.plot(Ps, Ts, '-s')
+plt.xlabel("Contact pressure (N/mm2)")
+plt.ylabel("Torque (N)")
+plt.tight_layout()
+
+plt.grid()
 plt.show()
